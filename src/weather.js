92f8c589189d54c8e7e7cb75076cb8b7827d9b47
JoyM <joy.js.mun@gmail.com>
2021-09-05 15:16:05 +0900

    ADD: weather function and css

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,8 @@
 const API_KEY = "6ab68981aee2954e9ffb336b6a2dfc02";
 const weatherIcon = document.querySelector("#weather-icon");
 let temp = document.querySelector("#temp");
+let mainWeather = '';
+let currentTemp = '';
 
 
 
@@ -9,11 +11,23 @@ function sucess(myPosition){
     let lat = crds.latitude;
     let lon = crds.longitude;
     const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    console.log(URL)
-    //fetch(URL).then(response => response.json()).then(data => console.log(data.temp,data.weather.icon))
    
- 
-
+    console.log(URL);
+    fetch(URL).then(response => response.json())
+    .then((data) =>
+        {
+            mainWeather = data.weather[0].main;
+            currentTemp = data.main.temp;    
+            temp.innerText = currentTemp;
+            const weather = new Array();
+            weather["Clear"] = "☀️";
+            weather["Clouds"] = "☁️";
+            weather["Rain"] = "🌦";
+            console.log(weather["clear"]);
+            console.log(mainWeather);
+            weatherIcon.innerText = weather[mainWeather]
+        })
+   
   
 }
 
